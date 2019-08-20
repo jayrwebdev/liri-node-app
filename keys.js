cd9b23442905313cc9d7b4ffc909f3c668ac4f37
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-08-19 21:46:31 -0400

    functionality to spotify and movie functions

--- keys.js
+++ keys.js
@@ -1,7 +1,6 @@
 
-console.log('this is loaded');
-​
-exports.spotify = {
+console.log('this is loaded')
+module.exports.spotify = {
   id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  secret: process.env.SPOTIFY_SECRET,
 };
--- liri.js
+++ liri.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 
-//var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+console.log(keys)
+var Spotify = require("node-spotify-api")
+var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var fs = require("fs")
-
 var feature = process.argv[2];
 var input = process.argv[3];
 
-consumerInput(feature,input)
-function consumerInput(feature,input) {
-     if (feature == "concert-this") {
-        console.log(feature+ " " +input)
-        
+consumerInput(feature, input)
+function consumerInput(feature, input) {
+    if (feature == "concert-this") {
+        console.log(feature + " " + input)
     } else if (feature == "spotify-this-song") {
         console.log(input)
         getSongInfo(input)
@@ -24,33 +24,105 @@ function consumerInput(feature,input) {
     }
 }
 
-function getSongInfo(input){
+function getSongInfo(input) {
     spotify.search(
         {
             type: "track",
             query: input,
         },
-        function(error,data) {
-            if (error) {
-                fs.appendFile("random.txt", "there was an ",error)
-                console.log("there was an error",error)
-            } else {
-                var music = data.track.items;
-
-                for (var i = 0 ; i < music.length; i++) {
-                  fs.appendFile("random.txt","********** Song Information **********")
-                }
-            }
+        function (error, response) {
+            console.log(response)
         }
     )
 
 }
 
 function getMovieInfo(input) {
-    axios.get("http://www.omdbapi.com/?t="+ input + "&apikey=5d2a7568")
-    .then(response => console.log(response.data) 
-      
-    )
+    axios.get("http://www.omdbapi.com/?t=" + input + "&apikey=5d2a7568")
+        .then(function (response) {
+            var movie = response
+            var movies = movie.data
+            fs.appendFile("random.txt", "******************************", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log("******************************")
+            fs.appendFile("random.txt", " " + movies.Title + " ", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log(movies.Title)
+            fs.appendFile("random.txt", " " + movies.Year + " ", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log(movies.Year)
+            fs.appendFile("random.txt", " " + movies.imdbRating + " ", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log(movies.imdbRating)
+            fs.appendFile("random.txt", " " + movies.Ratings[1].Value + " ", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log(movies.Ratings[1].Value)
+            fs.appendFile("random.txt", " " + movies.Country + " ", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log(movies.Country)
+            fs.appendFile("random.txt", " " + movies.Language + " ", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log(movies.Language)
+            fs.appendFile("random.txt", " " + movies.Plot + " ", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log(movies.Plot)
+            fs.appendFile("random.txt", " " + movies.Actors + " ", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log(movies.Actors)
+            fs.appendFile("random.txt", "******************************", function (error) {
+                if (error) {
+                    console.log("Ooooopsie there was a problem")
+                } else {
+                    console.log("text was updated")
+                }
+            })
+            console.log("******************************")
+        })
+
 }
 
-   
